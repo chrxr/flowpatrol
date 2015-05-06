1856492474aa4212f94ed46cbf68c9b3fe1ead18
Chris Rogers <chrxr@hotmail.co.uk>
2015-05-06 20:49:53 +0100

    changing settings

--- flowpatrol/settings/base.py
+++ flowpatrol/settings/base.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'flowpatrol',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '',
         'HOST': 'localhost',  # Set to empty string for localhost.
         'PORT': '5432',  # Set to empty string for default.
         'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for
